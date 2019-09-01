cdb15387877cb2be1ad4cf4223ec6e8b049d7ffa
Muhammad Tifak <KingGenius5@users.noreply.github.com>
2019-09-01 10:36:17 -0700

    Started basic user input

---
+++ Main.py
@@ -0,0 +1,12 @@
+from random import *
+
+nouns = []
+verbs = []
+adjectives = []
+
+for i in range (6):
+    nounsInputted = input("Enter some nouns for this MadLibs exercise: ")
+    nouns.append(nounsInputted)
+    i+=1
+
+print(nouns)
